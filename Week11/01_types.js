6a561dce5d7903a9b12091bde66fe217d4652272
Edwin Sumalabe <edwin.sumalabejr@benilde.edu.ph>
2024-07-23 09:43:33 +0800

    Cloned repo

---
+++ Week11/01_types.js
@@ -0,0 +1,8 @@
+// The undefined type 
+let message;
+console.log(message == undefined);
+console.log(typeof message);
+
+let messageNull = null;
+console.log(messageNull == null);
+console.log(typeof messageNull);
---
+++ Week11/04_eNotation.js
@@ -0,0 +1,6 @@
+let floatNum = 3.125e7; // equal to 31250000
+let floatNum2 = 3e-2; // equal to 0.05
+
+console.log (`The value of floatNum1 is ${floatNum}`)
+console.log (`The value of floatNum2 is ${floatNum2}`)
+
