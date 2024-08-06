265146bb26f4d7ff40af80c27b9f9317f7b07b4f
Edwin Sumalabe Jr. <edwin.sumalabejr@benilde.edu.ph>
2024-08-06 22:41:39 +0800

    Created Week13
    deliverable09 completed and Week12 server.js modified

--- Week12/nodeServer/server.js
+++ Week12/nodeServer/server.js
@@ -2,11 +2,12 @@ const { createServer } = require('node:http');
 
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3000; 
 
 const server = createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'application/json')
+    //res.setHeader('Content-Type', 'text/plain');
     res.end(JSON.stringify({
         'Manila': {
             '1000': 'cloudy',
